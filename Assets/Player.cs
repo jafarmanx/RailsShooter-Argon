e9ddcb3c1a1065d853ef1ae2502ff0b0afacb8d6
jafarmanx <ahsenjaffery007@gmail.com>
2018-09-01 12:40:59 -0500

    Initital Commit, with ship, circuit, camera parented to ship, and bullets and explosion particle systems.

---
+++ Assets/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("m/s ^-1")][SerializeField] float xSpeed = 4f;
+    [SerializeField] float xRange = 4f;
+
+    [Tooltip("m/s ^-1")] [SerializeField] float ySpeed = 4f;
+    [Tooltip("m")] [SerializeField] float yRange = 4f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float throwPitchFactor = -5f;
+
+    [SerializeField] float positionYawFactor = -5f;
+    [SerializeField] float throwRollFactor = -5f;
+
+    float xThrow;
+    float yThrow;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * throwPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * throwRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xRawPos = transform.localPosition.x + xOffset;
+        float xClamp = Mathf.Clamp(xRawPos, -xRange, xRange);
+
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float yRawPos = transform.localPosition.y + yOffset;
+        float yClamp = Mathf.Clamp(yRawPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(xClamp, yClamp, transform.localPosition.z);
+    }
+}
