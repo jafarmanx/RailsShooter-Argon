66bdda49a1c6443342f80911f958eba202140f21
jafarmanx <ahsenjaffery007@gmail.com>
2018-09-21 18:37:33 -0500

    Added Enemy script to be able to add collider

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour {
+
+
+    private void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider enemyCollider = gameObject.AddComponent<BoxCollider>();
+        enemyCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+	{
+		StartDeathSequence();
+	}
+
+	private void StartDeathSequence()
+	{
+		Destroy(gameObject);
+	}
+
+}
